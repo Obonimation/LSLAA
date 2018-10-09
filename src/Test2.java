55c592a2186676e596b1e638599df2fbae09cec5
Obonimation <obonimation@yandex.ru>
2018-10-10 01:27:14 +0300

    lslaa

--- src/Test2.java
+++ src/Test2.java
@@ -4,10 +4,11 @@ public class Test2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Лиза пахнет жопами?");
-        String value = scanner.next();
-        while(!value.equals("Да")){
-        System.out.println("Неверно");
-            value = scanner.next();}
+        String value = scanner.nextLine();
+        while (!"Да".equalsIgnoreCase(value)){
+            System.out.println("неверно");
+            value = scanner.nextLine();
+        }
         System.out.println("Верно :)");
 
     }

e3a4ccddcf461e0554c683963faceed67a091a18
Obonimation <obonimation@yandex.ru>
2018-10-13 14:10:09 +0300

    That was interesting :)

--- src/Comparison.java
+++ src/Comparison.java
@@ -1,4 +1,8 @@
 public class Comparison {
+    public static int min(int a, int b, int c, int d) {
+        int e = min(min(a, b), min(c, d));
+        return e;
+    }
     public static int min(int a, int b, int c) {
         if (a < b) {
             if (c < b) {
@@ -8,12 +12,22 @@ public class Comparison {
         else if (b < c) {return b;}
         else return c;
             }
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else return b;
+    }
 
     public static void main(String[] args) {
-        System.out.println(min(1, 2, 3));
-        System.out.println(min(-1, -2, -3));
+        System.out.println(min(1, 2));
+        System.out.println(min(-2, -3));
+        System.out.println(min(7, 5, 3));
         System.out.println(min(3, 5, 1));
         System.out.println(min(5, 7, 10));
+        System.out.println(min(1,2,3,4));
+        System.out.println(min(4,3,2,1));
+        System.out.println(min(1,4,-2,0));
+        System.out.println(min(-1,-7,0,6));
     }
 
 }
